3e0e9eb0bc9c6871f066c4098bde085e2988424c
MRSAE <MRSAE@DESKTOP-F1QCRBJ>
2023-06-01 23:37:00 +0100

    Ajoutez des fichiers projet.

---
+++ Sending/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sending
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 1;
+            while (i >= 10)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+
+                    Console.WriteLine("Incrementation",j);
+                }i++;
+            }
+        }
+    }
+}
